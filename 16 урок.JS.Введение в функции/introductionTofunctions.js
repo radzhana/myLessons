b9165718a84a25b66d45e2d237c9f6facd52460a
RadzhanaB <radzhana_1010@mail.ru>
2024-05-15 14:35:12 +0800

    modules

--- 16 урок.JS.Введение в функции/introductionTofunctions.js
+++ 16 урок.JS.Введение в функции/introductionTofunctions.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
 
-    <script>
       function min_value(a, b) {
         if (a < b) {
           return `Мешньшее число это ${a}`;
@@ -166,7 +159,3 @@
         console.log(`Доход от блюда ${dish.name}: ${profit}`);
       });
       console.log(menu)
-    </script>
-  </head>
-  <body></body>
-</html>
